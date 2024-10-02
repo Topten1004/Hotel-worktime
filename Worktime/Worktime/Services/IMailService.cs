c78a71bcbfa3f68922f3954fae730ce89d1eeaad
Topten1004 <crazycoding330@gmail.com>
2024-10-02 14:21:33 +0900

    initial version

---
+++ Worktime/Worktime/Services/IMailService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Worktime.Models;
+using Worktime.ViewModel;
+
+namespace Worktime.Services
+{
+    public interface IMailService
+    {
+        Task SendMajReport(MajReportRequest request);
+
+        Task SendForgotPassword(ForgotPasswordVM request);
+    }
+}
